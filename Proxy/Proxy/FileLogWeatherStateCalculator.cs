058fe3c90dccd8e2d90a781461532ea0c5c033ef
Aliakbar <77.timelord.77@gmail.com>
2019-06-26 21:32:28 +0600

    Прокси готов

---
+++ Proxy/Proxy/FileLogWeatherStateCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Proxy
+{
+    public class FileLogWeatherStateCalculator : ICalculator
+    {
+        private readonly ICalculator _calculator;
+
+        public FileLogWeatherStateCalculator(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public double CalcClouds()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            double result = _calculator.CalcClouds();
+            sw.Stop();
+
+            File.AppendAllText("log.txt", " CalcClouds elapsed: " + sw.ElapsedMilliseconds + " ");
+
+            return result;
+        }
+
+        public double CalcWind()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            double result = _calculator.CalcWind();
+            sw.Stop();
+
+            File.AppendAllText("log.txt", "CalcWind elapsed: " + sw.ElapsedMilliseconds + " ");
+
+            return result;
+        }
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TemperaturePredictor predictor = new TemperaturePredictor();
+            predictor.Calculator = new WeatherStateCalculator();
+
+            var temperature = predictor.PredictTemperature();
+            Console.WriteLine(temperature);
+
+            string[] config = File.ReadAllLines("config.txt", Encoding.Default);
+            foreach (string logType in config)
+            {
+                switch (logType)
+                {
+                    case "file":
+                        predictor.Calculator = new FileLogWeatherStateCalculator(new WeatherStateCalculator());
+                        predictor.PredictTemperature();
+                        break;
+
+                    case "console":
+                        predictor.Calculator = new ConsoleLogWeatherStateCalculator(new WeatherStateCalculator());
+                        predictor.PredictTemperature();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+    }
+}
